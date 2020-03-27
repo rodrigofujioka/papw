af17ea9a75a0b8dcc29dd165b7537ec6eb82433d
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2020-03-27 20:03:18 -0300

    Incluindo DTO básico na aula do dia 27/03/2020

--- src/main/java/com/rodrigofujioka/dev/web/rest/QuarentenaRest.java
+++ src/main/java/com/rodrigofujioka/dev/web/rest/QuarentenaRest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rodrigofujioka.dev.web.domain.Quarentena;
 import com.rodrigofujioka.dev.web.repository.QuarentenaRepository;
 import com.rodrigofujioka.dev.web.service.QuarentenaService;
+import com.rodrigofujioka.dev.web.service.dto.QuarentenaNomeCidadeDTO;
 
 //www.rodrigofujioka.com
 //localhost:8080
@@ -24,27 +27,63 @@ import com.rodrigofujioka.dev.web.service.QuarentenaService;
 @RestController
 @RequestMapping("/api")
 public class QuarentenaRest {
-	
+
 	@Autowired
 	private QuarentenaService quarentenaService;
-	//private QuarentenaRepository quarentenaRepository;
-		
+	// private QuarentenaRepository quarentenaRepository;
+
 	@GetMapping("/quarentena/{estado}")
-	public List<Quarentena> getQuarentena(@PathVariable  String estado) {
-		return quarentenaService.getQuarentenaByUf(estado);
+	public ResponseEntity<List<Quarentena>> getQuarentena(@PathVariable String estado) {
+		return ResponseEntity.ok(quarentenaService.getQuarentenaByUf(estado));
 	}
 
 	@PostMapping("/quarentena")
-	public Quarentena salvar(@RequestBody @Valid Quarentena quarentena) {
+	public ResponseEntity<Quarentena> salvar(@RequestBody @Valid Quarentena quarentena) {
 		Quarentena quarentenaSalva = quarentenaService.salvar(quarentena);
-		return quarentenaSalva;
+		return ResponseEntity.ok().body(quarentenaSalva);
 	}
-	
+
 	@GetMapping("/quarentena")
-	public List<Quarentena> getQuarentena(){
-		return quarentenaService.getQuarentena();
+	public ResponseEntity<List<Quarentena>> getQuarentena() {
+		return ResponseEntity.ok(quarentenaService.getQuarentena());
+	}
+
+	@GetMapping("/quarentena/{id}")
+	public ResponseEntity<Quarentena> getQuarentenaById(@PathVariable Long id) {
+		try {
+			
+			return ResponseEntity.ok(quarentenaService.getQuarentenaById(id));
+			
+		} catch (Exception e) {
+			// FIXME INCLUIR LOGS
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+	
+	@GetMapping("/quarentena/nome/cidade/{id}")
+	public ResponseEntity<QuarentenaNomeCidadeDTO> getQuarentenaNomeCidadeById(@PathVariable Long id) {
+		try {			
+			return ResponseEntity.ok(quarentenaService.getQuarentenaNomeCidadeById(id));			
+		} catch (Exception e) {
+			// FIXME INCLUIR LOGS
+			return ResponseEntity.notFound().build();
+		}
+
 	}
 	
 	
+	
+
+	@DeleteMapping("/quarentena/{id}")
+	public ResponseEntity deleteById(@PathVariable Long id) {
+		try {
+			quarentenaService.deleteQuarentenaById(id);
+		} catch (Exception e) {
+			// FIXME INCLUIR LOGS
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().build();
+	}
 
 }
